da3e41ee7125171639c4885ee36341a47ddf7e79
germanov <s.p.germanov@gmail.com>
2018-04-18 15:12:02 +0300

    change structure

--- myscript.js
+++
@@ -1,9 +0,0 @@
-function showH1Text(){
-    var content = document.querySelector('h1').textContent;
-    document.querySelector('h1').textContent = `${content} бе майна!`;
-}
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    chrome.tabs.executeScript(activeInfo.tabId, {code: '(' + showH1Text + ')();'}, function(response) {
-
-    });
-});
--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.send();
 
         var result = JSON.parse(xhr.responseText);
-        console.log(result);
+
         document.querySelectorAll(".true-news .counter")[0].innerHTML = result[0].field_true_news;
         document.querySelectorAll(".half-true-news .counter")[0].innerHTML = result[0].field_half_true_news ? result[0].field_half_true_news : 0;
         document.querySelectorAll(".fake-news .counter")[0].innerHTML = result[0].field_fake_news;
